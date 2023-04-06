e0bdc80c13f3a3ba9e9969c616746db02e22608b
zengzhiying <yingzhi_zeng@126.com>
2023-04-06 21:39:28 +0800

    Gin 示例代码添加多文件上传代码以及字节转 multipart.FileHeader 的方法

--- go/gin-example/example.go
+++ go/gin-example/example.go
@@ -1,9 +1,14 @@
 package main
 
-import "os"
-import "io"
-import "fmt"
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CustomMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -60,6 +65,41 @@ func main() {
 		c.File("./example.jpg")
 	})
 
+	// 多文件上传
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	r.POST("/multiple-upload", func(c *gin.Context) {
+		form, err := c.MultipartForm()
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(400, gin.H{"err": err})
+			return
+		}
+		formFiles := form.File
+		for key, files := range formFiles {
+			fmt.Printf("key: %s number of files: %d\n", key, len(files))
+			for _, f := range files {
+				fmt.Printf("file: %s size: %d\n", f.Filename, f.Size)
+			}
+		}
+		c.JSON(201, gin.H{"message": "ok"})
+	})
+
+	// b := []byte("this is text.")
+	// fileHeader, err := MultipartFormHeaderFromBytes(b)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// f, err := fileHeader.Open()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// d, err := io.ReadAll(f)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(string(d))
+
 
 	// 中间件
 	// curl -XPOST 'localhost:8080/cm' -d 'name=zzy'
@@ -89,4 +129,30 @@ func main() {
 	}
 
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+// 包装字节切片为 multipart.FileHeader 对象
+func MultipartFormHeaderFromBytes(src []byte) (*multipart.FileHeader, error) {
+	var buf bytes.Buffer
+	writer := multipart.NewWriter(&buf)
+	fw, err := writer.CreateFormFile("buffered_form", "buffered_file")
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(fw, bytes.NewReader(src))
+	if err != nil {
+		return nil, err
+	}
+	writer.Close()
+
+	reader := multipart.NewReader(bytes.NewReader(buf.Bytes()), writer.Boundary())
+	fm, err := reader.ReadForm(128 * 1024 * 1024)
+	if err != nil {
+		return nil, err
+	}
+	fileHeaders := fm.File["buffered_form"]
+	if fileHeaders != nil {
+		return fileHeaders[0], nil
+	}
+	return nil, fmt.Errorf("file headers is nil")
 }
